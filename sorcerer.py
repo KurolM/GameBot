a1213f7d19f3fe0c22174aba8a9633f8b3f4d66d
femik <femiking@gmx.de>
2020-03-23 00:30:19 +0000

    refactored sorceror

--- sorcerer.py
+++ sorcerer.py
@@ -1,19 +1,24 @@
-def sorcerer(level):
-    level = input()
+def sorcerer(level=None):  # optional argument with a default value
+    if level is None:
+        level_string = input("Enter level: ")
+        try:
+            level = int(level_string)
+        except ValueError:
+            print(f'You entered {level_string}, which is not a valid integer.')
+            return sorcerer()
+
+    if level < 8:
+        print('You need to be at least level 8')
+        return None, None
+
     hp = 185
     mana = 35
     hp_per_level = 5
     mana_per_level = 30
 
-    if int(level) == 8:
-        print("HP and MANA at lvl 8: ")
-        print(hp)
-        print(mana)
-    elif int(level) > 8:
-        print("HP and MANA at lvl " + level + ": ")
-        print(hp + hp_per_level * (int(level) - 8))
-        print(mana + mana_per_level * (int(level) - 8))
-    elif int(level) < 8:
-        print('You need to be at least level 8')
+    calculated_hp = hp + hp_per_level * (level - 8)
+    calculated_mana = mana + mana_per_level * (level - 8)
 
+    print(f'HP:{calculated_hp} and MANA:{calculated_mana} at lvl:{level}.')
 
+    return calculated_mana, calculated_hp

d19f1481b7f85e9dc2672e600fda39d6c7a3e2e7
femik <femiking@gmx.de>
2020-03-23 22:16:42 +0000

    refactored check image

--- imageChecker.py
+++ imageChecker.py
@@ -4,8 +4,15 @@ from PIL import Image
 def check_image():
     x = 47
     y = 7
+
     im = Image.open('Enter_game.jpg')  # Can be many different formats.
     pix = im.load()
+
+    magic_pixel = pix[x, y]
+    valid_colors = [
+        (116, 94, 254),
+        (115, 95, 252)
+    ]
 
     print(im.size)  # Get the width and height of the image for iterating over
     print(pix[x, y])  # Get the RGBA Value of the a pixel of an image
@@ -13,7 +20,15 @@ def check_image():
     im.save('Enter_game.jpg')  # Save the modified pixels as .png
 
     """If the 47, 7 pixel is (116, 94, 254) coloured"""
-    if pix[47, 7] == (116, 94, 254) or pix[47, 7] == (115, 95, 252):
+    if magic_pixel in valid_colors:
         print("utevo lux")
     else:
         print("not yet")
+
+
+def selected_pixel_in_color_list(image_file, x, y, color_list):
+    im = Image.open(image_file)
+    image_pixels = im.load()
+    selected_pixel = image_pixels[x, y]
+
+    return selected_pixel in color_list

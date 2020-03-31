a18a57b8f88641789a72ee93b5143f386b901d32
femik <femiking@gmx.de>
2020-03-31 04:00:08 +0100

    refactored sorceror

--- sorcerer.py
+++ sorcerer.py
@@ -1,24 +1,29 @@
 def sorcerer(level=None):  # optional argument with a default value
     if level is None:
-        level_string = input("Enter level: ")
-        try:
-            level = int(level_string)
-        except ValueError:
-            print(f'You entered {level_string}, which is not a valid integer.')
-            return sorcerer()
+        level = get_valid_integer("Enter level: ")
 
-    if level < 8:
-        print('You need to be at least level 8')
-        return None, None
-
-    hp = 185
-    mana = 35
-    hp_per_level = 5
-    mana_per_level = 30
-
-    calculated_hp = hp + hp_per_level * (level - 8)
-    calculated_mana = mana + mana_per_level * (level - 8)
-
-    print(f'HP:{calculated_hp} and MANA:{calculated_mana} at lvl:{level}.')
+    calculated_mana, calculated_hp = get_calculated_hp_and_mana(level)
 
     return calculated_mana, calculated_hp
+
+
+def get_calculated_hp_and_mana(level, base_hp=185, base_mana=35, hp_per_level=5, mana_per_level=30, min_level=8):
+    if level < min_level:
+        print(f'You need to be at least level {min_level}')
+        return None, None
+
+    calculated_hp = base_hp + hp_per_level * (level - min_level)
+    calculated_mana = base_mana + mana_per_level * (level - min_level)
+
+    return calculated_mana, calculated_hp
+
+
+def get_valid_integer(prompt):
+    input_string = input(prompt)
+    try:
+        valid_int = int(input_string)
+    except ValueError:
+        print(f'You entered {input_string}, which is not a valid integer.')
+        return get_valid_integer(prompt)
+
+    return valid_int
